540b4de881d9a97219ec6e1f513430780cb58b7d
nakamarusun <48478366+nakamarusun@users.noreply.github.com>
2020-02-15 21:22:47 +0700

    Added basic ConfigParser to load conf file, and edited some settings.conf

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 # Main file
+
+from configparser import ConfigParser
+import pathlib
+
+# Create ConfigParser object to read conf file
+configFile = ConfigParser()
+
+# Read the conf file
+path = str(pathlib.Path().absolute())
+configFile.read( path + "\settings.conf" )
+
+# Assign variables
+updateInterval: int = int(configFile["DEFAULT"]["interval"])
