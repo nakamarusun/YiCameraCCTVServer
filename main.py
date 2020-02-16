7b2b7e56fc48cdb52d5d5879a2325f857cf23e08
nakamarusun <nakamarujc@gmail.com>
2020-02-16 12:37:32 +0700

    Added basic test for folder path, and create_dirs.py file. Added support for osSlash

--- main.py
+++ main.py
@@ -2,13 +2,43 @@
 
 from configparser import ConfigParser
 import pathlib
+import subprocess
+from platform import system
+
+if system == "Windows":
+    osSlash = "\"
+else:
+    osSlash = "/"
 
 # Create ConfigParser object to read conf file
 configFile = ConfigParser()
 
 # Read the conf file
-path = str(pathlib.Path().absolute())
-configFile.read( path + "\settings.conf" )
+path: str = str(pathlib.Path().absolute())
+configFile.read( path + osSlash + "settings.conf" )
 
 # Assign variables
+savePath: str = str(configFile["DEFAULT"]["savepath"])
+
 updateInterval: int = int(configFile["DEFAULT"]["interval"])
+folderPath: str = savePath + (osSlash if savePath[-1] != osSlash else "") + str(configFile["DEFAULT"]["savefolder"])
+
+
+
+# mkdir directories
+
+test = subprocess.Popen(['test', '-d', folderPath, '&&', 'echo', 'true', '||', 'echo', 'false'],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT)
+out, err = test.communicate()
+if out == 'true':
+    pass
+    # Don't create new directory
+else:
+    pass
+    # Create directory recursively
+
+
+# Cleanup
+del savePath
+del configFile
