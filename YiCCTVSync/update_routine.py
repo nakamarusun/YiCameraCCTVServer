2569b30936d4b7e5f5642e39f47098b046a0654d
nakamarusun <nakamarujc@gmail.com>
2020-02-22 18:41:00 +0700

    Experimental update_routine almost complete. Added filediff

--- YiCCTVSync/update_routine.py
+++ YiCCTVSync/update_routine.py
@@ -5,6 +5,7 @@ from time import time
 
 from os import listdir
 import string
+from create_dirs import createDirectory
 
 def getFolderDetail(cameraList) -> str:
     try:
@@ -39,27 +40,45 @@ def updateRoutine():
         if verbose: print("{name}: {ftpmsg}".format( name, ftp.login(user=cams.getLogin, passwd=cams.getPasswd) ))
         if verbose: print("{name}: {ftpmsg}".format( name, ftp.cwd(cctvDir)))
 
+        # Folders dictionary format:
+        # ******Folders:dict = {folderDirectory: [files]}
         # Check server's folder for files list
 
-        if verbose: print("\n{name} Listing all file from server..".format(name))
+        if verbose: print("\n{name}: Listing all file from server..".format(name))
         camServerFolders: dict = {}
+
+        # For folders in the directory of camera (ex. 2020Y02M19D13H')
         for fold in listdir(cams.getFolderName).sort():
             camServerFolders[fold] = listdir(cams.getFolderName + osSlash + fold)
 
         if verbose: print(getFolderDetail(list(camServerFolders.keys())))
 
-
         # Check camera's folder for files list
 
-        if verbose: print("\n{name} Listing all file from camera..".format(name))
+        if verbose: print("\n{name}: Listing all file from camera..".format(name))
 
         camCCTVFolders: dict = {}
-        for fold in filter(lambda fold : fold != "tmp.mp4.tmp" , ftp.nlst('/tmp/sd/record') ).sort():
+        for fold in filter(lambda fold : fold != "tmp.mp4.tmp" , ftp.nlst( cctvDir ) ).sort():
             camCCTVFolders[fold] = ftp.nlst(cctvDir + "/" + fold)
 
         if verbose: print(getFolderDetail(list(camCCTVFolders.keys())))
 
         # Download whats not on the server's file list
 
-        if verbose: print("\n Searching for file difference...")
+
+        if verbose: print("\n{name}: Searching for file difference...".format(name))
         fileDiffs: list = []
+
+        for camDir in camCCTVFolders:
+            # Current server directory camera (ex. foldername/2020Y02M19D13H)
+            currentServerDir = cams.getFolderName + osSlash + camDir.split
+            # If camera directory is not already initialized in server, make it.
+            if camDir not in list( camServerFolders.keys() ): createDirectory( currentServerDir )
+
+            # Checks for files in the directory
+            for camFile in camCCTVFolders[camDir]:
+                if camFile not in currentServerDir:
+                    fileDiffs.append( cctvDir + "/" + camDir + "/" + camFile )
+
+        if verbose: print("\n{name}: The difference in files are:\n".format(name), fileDiffs)
+        if verbose: print("{name}: Operation complete.".format(name))
